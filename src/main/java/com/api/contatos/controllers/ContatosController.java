e77fb2e34dec819e9e6d271a0215c3830fa07233
Rodnei Miranda da Silva <eletrodnei@hotmail.com>
2020-02-08 20:27:54 -0300

    inserido update

--- src/main/java/com/api/contatos/controllers/ContatosController.java
+++ src/main/java/com/api/contatos/controllers/ContatosController.java
@@ -3,21 +3,27 @@ package com.api.contatos.controllers;
 import java.util.List;
 import java.util.Optional;
 
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.aop.ThrowsAdvice;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.contatos.entities.ContatoEntity;
 import com.api.contatos.interfaces.ContatoInterface;
 
+import javassist.NotFoundException;
+import net.bytebuddy.implementation.bytecode.Throw;
+
 @RestController
 public class ContatosController {
-	
+
 	@Autowired
 	ContatoInterface contatointerface;
 
@@ -40,4 +46,19 @@ public class ContatosController {
 		contatointerface.delete(procurar.get());
 	}
 
+	@PutMapping("/{ID}")
+	public ContatoEntity AtualizarContato(@PathVariable long ID, @RequestBody ContatoEntity entity)
+			throws NotFoundException {
+		Optional<ContatoEntity> procurar = contatointerface.findById(ID);
+		if (procurar.isPresent()) {
+			ContatoEntity contatoEntity = procurar.get();
+			contatoEntity.setNome(entity.getNome());
+			contatoEntity.setTelefone(entity.getTelefone());
+			contatointerface.save(contatoEntity);
+			return contatoEntity;
+		}
+		throw new NotFoundException("Registro não encontrado !!!");
+	}
 }
+
+
