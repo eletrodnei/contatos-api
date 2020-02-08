d53b98ac27029e026efd93d8019db0fdcc619f8f
Rodnei Miranda da Silva <eletrodnei@hotmail.com>
2020-02-08 19:21:05 -0300

    inserido delete

--- src/main/java/com/api/contatos/controllers/ContatosController.java
+++ src/main/java/com/api/contatos/controllers/ContatosController.java
@@ -1,10 +1,13 @@
 package com.api.contatos.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.web.PageableDefault;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +17,7 @@ import com.api.contatos.interfaces.ContatoInterface;
 
 @RestController
 public class ContatosController {
+	
 	@Autowired
 	ContatoInterface contatointerface;
 
@@ -22,10 +26,18 @@ public class ContatosController {
 		Iterable<ContatoEntity> findAll = contatointerface.findAll();
 		return (List<ContatoEntity>) findAll;
 	}
+
 	@PostMapping
 	public ContatoEntity inserirContato(@RequestBody ContatoEntity entity) {
 		ContatoEntity contato = contatointerface.save(entity);
 		return contato;
+
+	}
+
+	@DeleteMapping("/{ID}")
+	public void deletarContato(@PathVariable long ID) {
+		Optional<ContatoEntity> procurar = contatointerface.findById(ID);
+		contatointerface.delete(procurar.get());
 	}
 
 }
